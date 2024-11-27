c422428aeefaeaf1dc7ddb45c404168cfad5fb6b
Sebastián Maldonado <98917583+SebastianMaldonado@users.noreply.github.com>
2024-11-26 22:08:53 -0500

    Useless Endpoints Removed

--- usercrud/urls.py
+++ usercrud/urls.py
@@ -13,12 +13,9 @@ from rest_framework_simplejwt.views import (
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'user', UserProfileViewSet, basename='Image')
 router.register(r'logs', LogViewSet, basename="Log")
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
 
